2dcd8eac73a60c0286cf5113a50ff4d47976f076
codeBlack5 <stevekoech8@gmail.com>
2022-11-29 18:04:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const namesArray = [];
+
+function writeCards(namesArray, surprise) {
+    for (let i=0; i<namesArray.length; i++) {
+        namesArray[i] =`Thank you, ${namesArray[i]}, for the wonderful ${surprise} gift!`}
+    return namesArray;
+    }
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+function countDown (number){
+    while (number>=0){
+        console.log(number--);
+        debugger;
+    }
+}
+countDown(10);
